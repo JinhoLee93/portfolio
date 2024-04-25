91c9993265c4abbc7c75680222689fcc9516e715
Jinho Lee <jl5027@columbia.edu>
2024-04-25 18:50:38 +0900

    [Feature] Implemented NSCache for images

--- PokeDex (포켓몬 도감)/PokeDex/PokeDex/Cache/MemoryCacheManager.swift
+++ PokeDex (포켓몬 도감)/PokeDex/PokeDex/Cache/MemoryCacheManager.swift
@@ -6,11 +6,35 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
 
-final class MemoryCacheManager: NSCache<AnyObject, AnyObject> {
+final class MemoryCacheManager {
     static let shared = MemoryCacheManager()
     
-    private override init() { }
+    private init() { }
     
+    var imageCache: NSCache<NSString, UIImage> = {
+        
+        // Customize your cache
+        let cache = NSCache<NSString, UIImage>()
+        
+        cache.countLimit = 100
+        cache.totalCostLimit = 1024 * 1024 * 100
+        
+        return cache
+    }() // Key-Value Dictionary-like Data Structure
     
+    func addToCache(image: UIImage, name: String) {
+        imageCache.setObject(image, forKey: name as NSString)
+    }
+    
+    func removeFromCache(name: String) {
+        imageCache.removeObject(forKey: name as NSString)
+    }
+    
+    func getImage(name: String) -> UIImage? {
+        
+        return imageCache.object(forKey: name as NSString)
+    }
 }
