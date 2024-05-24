c306529468a15ab248f42f61bd56ec150d1535fc
Jinho Lee <jl5027@columbia.edu>
2024-05-24 10:59:36 +0900

    [Client] Finishing up client side POST request functions.

--- Beatitudo Media/BeatitudoMedia/BeatitudoMedia/Models/BeatitudoMediaUser.swift
+++ Beatitudo Media/BeatitudoMedia/BeatitudoMedia/Models/BeatitudoMediaUser.swift
@@ -8,14 +8,12 @@
 import Foundation
 
 struct BeatitudoMediaUser: Codable, Hashable {
-    let id: UUID
     let email: String
     let nickname: String
     let viewedArticles: [Article]
     let likedArticles: [Article]
     
     enum CodingKeys: String, CodingKey {
-        case id
         case email
         case nickname
         case viewedArticles = "viewed_articles"
